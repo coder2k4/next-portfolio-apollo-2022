af57000842d6b041b170cb5b1aac340a70dec8f1
Alexey G. Standalone2k <coder2k@mail.ru>
2022-04-30 12:28:27 +0300

    Рефакторинг страниц. Вынесен Navbar в _app

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,8 @@
-import Navbar from '@/components/shared/Navbar';
-import Hero from '@/components/shared/Hero';
+
+
 
 const Home = () => (
-  <div className="portfolio-app">
-    <Navbar />
-    <Hero />
-    <div className="container">
+    <>
       {/* HOME PAGE STARTS */}
       <section className="section-title">
         <div className="px-2">
@@ -101,56 +98,55 @@ const Home = () => (
       </section>
       <a href="" className="btn btn-main bg-blue ttu">See More Posts</a>
       {/* HOME PAGE ENDS */}
-    </div>
 
-    {/* REPLIER STARTS */}
-    {/* <div className='reply-controls is-open'> */}
-    <div className='reply-controls'>
-      <div className="reply-area">
-        <div className="reply-to">
-          Reply To: <span className="text ml-2">User1</span>
-        </div>
-        <div className="fj-editor-input">
-          <input
-            name="title"
-            placeholder="Topic title"
-            type="text"></input>
-        </div>
-        <div className="fj-editor">
-          <div className="fj-editor-textarea-wrapper">
-            <textarea
-              name="content"
-              placeholder="Type here">
-            </textarea>
+      {/* REPLIER STARTS */}
+      {/* <div className='reply-controls is-open'> */}
+      <div className='reply-controls'>
+        <div className="reply-area">
+          <div className="reply-to">
+            Reply To: <span className="text ml-2">User1</span>
           </div>
-          <div className="fj-editor-preview-wrapper">
-            <div className="preview">
-              <p></p>
+          <div className="fj-editor-input">
+            <input
+                name="title"
+                placeholder="Topic title"
+                type="text"></input>
+          </div>
+          <div className="fj-editor">
+            <div className="fj-editor-textarea-wrapper">
+            <textarea
+                name="content"
+                placeholder="Type here">
+            </textarea>
+            </div>
+            <div className="fj-editor-preview-wrapper">
+              <div className="preview">
+                <p></p>
+              </div>
+            </div>
+          </div>
+          <div className="submit-area">
+            <div className="send mr-auto">
+              <button
+                  href="#"
+                  className="btn btn-main bg-blue py-2 ttu">Reply</button>
+              <a className="btn py-2 ttu gray-10">Cancel</a>
+            </div>
+            <div>
+              <a className="btn py-2 ttu gray-10">hide preview</a>
             </div>
           </div>
         </div>
-        <div className="submit-area">
-          <div className="send mr-auto">
-            <button
-              href="#"
-              className="btn btn-main bg-blue py-2 ttu">Reply</button>
-            <a className="btn py-2 ttu gray-10">Cancel</a>
-          </div>
-          <div>
-            <a className="btn py-2 ttu gray-10">hide preview</a>
-          </div>
+      </div>
+      {/* REPLIER ENDS */}
+      {/* FOOTER STARTS */}
+      <footer id="sticky-footer" className="py-4 bg-black text-white-50 py-3">
+        <div className="container text-center">
+          <small>Copyright &copy; Your Website</small>
         </div>
-      </div>
-    </div>
-    {/* REPLIER ENDS */}
-    {/* FOOTER STARTS */}
-    <footer id="sticky-footer" className="py-4 bg-black text-white-50 py-3">
-      <div className="container text-center">
-        <small>Copyright &copy; Your Website</small>
-      </div>
-    </footer>
-    {/* FOOTER ENDS */}
-  </div>
+      </footer>
+      {/* FOOTER ENDS */}
+    </>
 )
 
 export default Home

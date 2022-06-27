48ad747b982f95b043bc4d23b8cf644a3a21f121
Alexey G. Standalone2k <coder2k@mail.ru>
2022-06-27 16:19:03 +0300

    Инициализация GraphQl

--- server/index.js
+++ server/index.js
@@ -8,16 +8,44 @@ const hostname = 'localhost'
 const app = next({dev, hostname, port})
 const handle = app.getRequestHandler()
 
+// Setup GraphQL
+const {graphqlHTTP} = require('express-graphql');
+const {buildSchema} = require('graphql')
+
+
 app.prepare().then(() => {
 
     const server = express()
+
+    const schema = buildSchema(`
+        type Query {
+            hello: String
+        }
+    `)
+
+    const root = {
+        hello: () => {
+            return "Hello, World"
+        },
+    }
+
+
+    server.use(
+        "/graphql",
+        graphqlHTTP({
+            schema: schema,
+            rootValue: root,
+            graphiql: true
+        })
+    );
+
 
     server.all('*', (req, res) => {
         return handle(req, res)
     })
 
-    server.listen(port, err =>{
-        if(err) throw err
+    server.listen(port, err => {
+        if (err) throw err
         console.log(`> Ready on http://${hostname}:${port}`)
     })
 
